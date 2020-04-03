f7aa568bdc148cd3bf1facee4f4df110bc7a96ac
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2020-04-03 15:00:00 -0300

    Adicionado referências para PostgreSQL

--- WebAppClientes/WebAppClientes.Infra.CrossCutting.Ioc/IocBootstrapper.cs
+++ WebAppClientes/WebAppClientes.Infra.CrossCutting.Ioc/IocBootstrapper.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppClientes.Infra.Data;
 using WebAppClientes.Repositories;
 using WebAppClientes.Services;
 
@@ -6,8 +9,11 @@ namespace WebAppClientes.Infra.CrossCutting.Ioc
 {
     public class IocBootstrapper
     {
-        public static void RegisterServices(IServiceCollection services)
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<IClienteServices, ClienteServices>();
             services.AddScoped<IClienteRepository, ClienteRepository>();
         }
--- WebAppClientes/WebAppClientes.Ui.Web/Startup.cs
+++ WebAppClientes/WebAppClientes.Ui.Web/Startup.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebAppClientes.Infra.CrossCutting.Ioc;
 
 namespace WebAppClientes.Ui.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IHostEnvironment env)
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
+
+            IocBootstrapper.RegisterServices(services, _configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -20,6 +36,8 @@ namespace WebAppClientes.Ui.Web
             }
 
             app.UseRouting();
+
+            app.UseHealthChecks("/check");
 
             app.UseEndpoints(endpoints =>
             {
